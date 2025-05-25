9c6158fd62543582b2ce38f56a13e244b42d1bd2
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-25 17:06:54 +0800

    路由初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,13 @@
-import { Col, Row, Typography } from 'antd';
+import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
-import sideImage3 from './assets/images/sider_2019_02-04-2.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage from './assets/images/sider_2019_12-09.png';
-import {
-  BusinessPartners,
-  Carousel,
-  Footer,
-  Header,
-  ProductCollection,
-  SideMenu,
-} from './components';
-import { productList1, productList2, productList3 } from './mockups';
+import { HomePage } from './page';
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      {/* 页面内容 content */}
-      <div className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              爆款推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              国内游推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
-        <BusinessPartners />
-      </div>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
